7bfef3d394258c8bc905d13e9c1a74d8ffdd5aa8
“Ashar1999” <f180391@nu.edu.pk>
2022-10-30 01:48:26 +0500

    I have updated with comments

--- jacked array.cpp
+++ jacked array.cpp
@@ -2,12 +2,14 @@
 using namespace std;
 int main()
 {
+	//intializing variables and print statement
 	int row, col; 
 	int **ptr, *backup;
 	cout << "Enter rows: ";
 	cin >> row;
 	ptr = new int *[row];
 	backup = new int[row];
+	//for looping to entering user define data
 	for (int i = 0; i < row; i++)
 	{
 		cout << "Enter columns: ";
@@ -21,6 +23,7 @@ int main()
 	}
 	cout << endl;
 	cout<<"The output of Jacked array is: " << endl;
+	//for looping for showing data in array
 	for (int i = 0; i < row; i++)
 	{
 		for (int j = 0; j < backup[i]; j++)
